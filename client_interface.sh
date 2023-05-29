387361f6fc81332b60c7ee87a2382103fbdd8822
ltorrusio <ltorrusio@Ubuntu.myguest.virtualbox.org>
2023-05-29 20:39:21 -0300

    Client

--- client_interface.sh
+++ client_interface.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "Server Public key: $1"
+echo "server private network: $2"
+echo "Server Public ip: $3"
+
 sudo ip link add dev wg0 type wireguard
 # $1 -> server pub key - $2 -> server private network - $3 -> server pub ip
-sudo wg set wg0 private-key ./keys/private.key peer $1 allowed-ips $2 endpoint $3:51820
+sudo wg set wg0 private-key privatekey peer $1 allowed-ips $2 endpoint $3:51820
 sudo wg setconf wg0 wg0.conf
 sudo ip link set up dev wg0
 sudo wg-quick up wg0
-echo "Server Public key: $1"
