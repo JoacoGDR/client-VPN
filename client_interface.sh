aad5bbb77b32a1218f69a5c37f41f2078928bb82
Lucia <lucia.torrusio@gmail.com>
2023-05-29 22:37:09 -0300

    Update client_interface.sh

--- client_interface.sh
+++ client_interface.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-
 echo "Server Public key: $1"
-
 echo "server private network: $2"
-
 echo "Server Public ip: $3"
 
-
-
-sudo ip link delete dev wg0
-
+sudo ip link delete dev wg0 > /dev/null
 sudo ip link add dev wg0 type wireguard
-
 sudo wg set wg0 private-key privatekey peer $1 allowed-ips $2 endpoint $3:51820
-
 sudo ip link set dev wg0 up
-
 sudo ip route add $2 dev wg0 scope link
